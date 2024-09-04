f70a4f469c40e42a227ecb16a45dd57ac060824c
Ageng Pangestu <agengpangstu99@gmail.com>
2024-09-04 10:44:17 +0700

    deploy

--- src/middlewares/cloudinary.ts
+++ src/middlewares/cloudinary.ts
@@ -25,16 +25,6 @@ export const uploadCloudinary = async (req: Request, res: Response, next: NextFu
   if (file) {
     return uploadSingle(file, res, next);
   }
-  // Kalau ada file muliple
-  // if (!file && files.length < 1) {
-  //   return next();
-  // }
-
-  // if (file) {
-  //   return uploadSingle(file, res, next);
-  // } else {
-  //   return uploadMultiple(files, res, next);
-  // }
 };
 
 const uploadMultiple = async (files: CloudinaryFile[], res: Response, next: NextFunction) => {
